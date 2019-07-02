f78840bbdf22fdd6a554b117b84018289dc58c74
fadyfouad <fady.fouad.a@gmail.com>
2019-07-02 23:55:19 +0200

    Initial commit

--- src/com/etaTech/Main.java
+++ src/com/etaTech/Main.java
@@ -1,7 +1,25 @@
 package com.etaTech;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("----------------HIgh Level API---------------------\n");
+        try {
+            URI uri = new URI("http://username:password@myserver.com:5000/catalogue/phones?os=android#Huawei");
+            System.out.println("Scheme = " + uri.getScheme());
+            System.out.println("Scheme-specific part = " + uri.getSchemeSpecificPart());
+            System.out.println("Authority = " + uri.getAuthority());
+            System.out.println("User info = " + uri.getUserInfo());
+            System.out.println("Host = " + uri.getHost());
+            System.out.println("Port = " + uri.getPort());
+            System.out.println("Path = " + uri.getPath());
+            System.out.println("Query = " + uri.getQuery());
+            System.out.println("Fragment = " + uri.getFragment());
+
+        } catch(URISyntaxException e) {
+            System.out.println("URI Bad Syntax: " + e.getMessage());
+        }
     }
 }

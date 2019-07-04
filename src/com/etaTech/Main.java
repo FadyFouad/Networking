1ad224038f31b2c20adf5dccb13720ac8eb2c147
fadyfouad <fady.fouad.a@gmail.com>
2019-07-04 22:03:37 +0200

    Initial commit

--- src/com/etaTech/Main.java
+++ src/com/etaTech/Main.java
@@ -1,25 +1,53 @@
 package com.etaTech;
 
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("----------------HIgh Level API---------------------\n");
         try {
             URI uri = new URI("http://username:password@myserver.com:5000/catalogue/phones?os=android#Huawei");
-            System.out.println("Scheme = " + uri.getScheme());
-            System.out.println("Scheme-specific part = " + uri.getSchemeSpecificPart());
-            System.out.println("Authority = " + uri.getAuthority());
-            System.out.println("User info = " + uri.getUserInfo());
-            System.out.println("Host = " + uri.getHost());
-            System.out.println("Port = " + uri.getPort());
-            System.out.println("Path = " + uri.getPath());
-            System.out.println("Query = " + uri.getQuery());
-            System.out.println("Fragment = " + uri.getFragment());
+
+//            URL url = uri.toURL();
+//            System.out.println(uri);
+//            System.out.println(url);
+
+
+            URL url = new URL("http://example.org");
+            URLConnection urlConnection = url.openConnection();
+            urlConnection.setDoOutput(true);
+            urlConnection.connect();
+
+            BufferedReader inputStream = new BufferedReader(
+                    new InputStreamReader(urlConnection.getInputStream()));
+
+            String line = "";
+            while(line != null) {
+                line = inputStream.readLine();
+                System.out.println(line);
+            }
+            inputStream.close();
+
+
+//            System.out.println("\nScheme = " + uri.getScheme());
+//            System.out.println("Scheme-specific part = " + uri.getSchemeSpecificPart());
+//            System.out.println("Authority = " + uri.getAuthority());
+//            System.out.println("User info = " + uri.getUserInfo());
+//            System.out.println("Host = " + uri.getHost());
+//            System.out.println("Port = " + uri.getPort());
+//            System.out.println("Path = " + uri.getPath());
+//            System.out.println("Query = " + uri.getQuery());
+//            System.out.println("Fragment = " + uri.getFragment());
 
         } catch(URISyntaxException e) {
             System.out.println("URI Bad Syntax: " + e.getMessage());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
